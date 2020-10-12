3fce9d69f34524bf3182c689eda4c0f40115a96e
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-12 21:07:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,8 @@ def introduction (name)
   puts "Hi my name is #{name}."
 end 
 
+puts (Kevin)
+
 def introduction_with_language (name, language)
  puts "Hi my name is #{name}, and I am learning to program in #{language}."
 end
